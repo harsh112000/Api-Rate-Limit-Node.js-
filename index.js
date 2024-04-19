9c304a9121c4f2e669fa94a56c128271de49da42
harsh1111 <harsh.srivastava@zoopindia.com>
2024-04-19 18:26:22 +0530

    used x-forwarded-for in place of req.ip

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const axios = require("axios");
-const rateLimit = require("express-rate-limit");
+// const express = require("express");
+// const axios = require("axios");
+// const rateLimit = require("express-rate-limit");
 
-const app = express();
+// const app = express();
 
-// Rate limit parameters
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 5 requests per windowMs
-  message: "Too many requests from this IP, please try again later",
-});
+// // Rate limit parameters
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 5, // limit each IP to 5 requests per windowMs
+//   message: "Too many requests from this IP, please try again later",
+// });
 
-// Apply rate limiter to all requests
-app.use(limiter);
+// // Apply rate limiter to all requests
+// app.use(limiter);
 
-// Endpoint to fetch random user data
-app.get("/random-user", async (req, res) => {
-  try {
-    const apiUrl = "https://randomuser.me/api/";
-    const response = await axios.get(apiUrl);
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error:", error.message);
-    res.status(500).send("Internal Server Error");
-  }
-});
+// // Endpoint to fetch random user data
+// app.get("/random-user", async (req, res) => {
+//   try {
+//     const apiUrl = "https://randomuser.me/api/";
+//     const response = await axios.get(apiUrl);
+//     res.json(response.data);
+//   } catch (error) {
+//     console.error("Error:", error.message);
+//     res.status(500).send("Internal Server Error");
+//   }
+// });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const axios = require("axios");
 
 const app = express();
 
-const WINDOW_SIZE = 15 * 60 * 1000; // 15 minutes
+const WINDOW_SIZE = 15 * 60 * 1000; 
 const MAX_RANDOM_USER_REQUESTS = 7;
+const map = new Map(); 
 const MAX_COIN_GECKO_REQUESTS = 5;
-const randomUserRequestMap = new Map(); // Map to store random user requests per IP
-const coinGeckoRequestMap = new Map(); // Map to store CoinGecko requests per IP
+const coinGeckoRequestMap = new Map(); 
 
 const getRandomUserData = async () => {
   try {
@@ -31,19 +31,21 @@ const getCoinGeckoData = async () => {
   }
 };
 
-const randomUserRateLimitMiddleware = (req, res, next) => {
-  const ip = req.ip;
+const apiRateLimit = (req, res, next) => {
+  // const ip = req.ip;
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  console.log(ip)
   const currentTimestamp = Date.now();
-  const requests = randomUserRequestMap.get(ip) || [];
-  console.log(randomUserRequestMap.get(ip));
+  const requests = map.get(ip) || [];
+  console.log(map.get(ip));
   console.log(requests, req.ip);
   requests.push(currentTimestamp);
-  randomUserRequestMap.set(ip, requests);
-  randomUserRequestMap.set(
+  map.set(ip, requests);
+  map.set(
     ip,
     requests.filter((timestamp) => timestamp > currentTimestamp - WINDOW_SIZE)
   );
-  if (randomUserRequestMap.get(ip).length > MAX_RANDOM_USER_REQUESTS) {
+  if (map.get(ip).length > MAX_RANDOM_USER_REQUESTS) {
     res.status(429).send("Too many requests for random user data from this IP, please try again later");
     return;
   }
@@ -51,7 +53,9 @@ const randomUserRateLimitMiddleware = (req, res, next) => {
 };
 
 const coinGeckoRateLimitMiddleware = (req, res, next) => {
-  const ip = req.ip;
+  // const ip = req.ip;
+  const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+
   const currentTimestamp = Date.now();
   const requests = coinGeckoRequestMap.get(ip) || [];
   requests.push(currentTimestamp);
@@ -67,7 +71,7 @@ const coinGeckoRateLimitMiddleware = (req, res, next) => {
   next();
 };
 
-app.use("/random-user", randomUserRateLimitMiddleware);
+app.use("/random-user", apiRateLimit);
 app.use("/coin-gecko", coinGeckoRateLimitMiddleware);
 
 app.get("/random-user", async (req, res) => {
